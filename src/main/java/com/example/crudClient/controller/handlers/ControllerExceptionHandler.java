1db690f8d6f31d77b79ed1cba64e8a64c799f437
Leandro Marques <leandrojerse@gmail.com>
2025-07-07 18:57:08 -0300

    ControllerExceptionHandler done

--- src/main/java/com/example/crudClient/controller/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/example/crudClient/controller/handlers/ControllerExceptionHandler.java
@@ -1,8 +1,41 @@
 package com.example.crudClient.controller.handlers;
 
+import com.example.crudClient.dto.errorDTO.CustomError;
+import com.example.crudClient.dto.errorDTO.ValidationError;
+import com.example.crudClient.service.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI() );
+        return ResponseEntity.status(status).body(err);
+
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inváliodos", request.getRequestURI() );
+
+        e.getBindingResult().getFieldErrors().forEach(f -> {
+            err.addError(f.getField(), f.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(status).body(err);
+    }
 }
+
+
+
+
--- src/main/java/com/example/crudClient/dto/clientDTO/ClientRequestDTO.java
+++ src/main/java/com/example/crudClient/dto/clientDTO/ClientRequestDTO.java
@@ -2,9 +2,10 @@ package com.example.crudClient.dto.clientDTO;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-public record ClientRequestDTO(@NotBlank String name, @NotBlank String cpf,
+public record ClientRequestDTO(@NotBlank @Size(min = 2, max = 50) String name, @NotBlank @Size(min = 11, max  = 11) String cpf,
                                Double income, @PastOrPresent LocalDate birthDate, Integer children) {
 }
